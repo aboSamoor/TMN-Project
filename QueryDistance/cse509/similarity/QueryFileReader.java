c3e69d81d72f909dcaaa47ea4aab9f50661e0650
Nikhil Patwardhan <nikhil@nikhil-ThinkPad-SL410.(none)>
2010-12-01 22:05:00 -0500

    Modified java for calling Rami

--- QueryDistance/cse509/similarity/QueryFileReader.java
+++ QueryDistance/cse509/similarity/QueryFileReader.java
@@ -5,10 +5,12 @@ import java.util.*;
 
 public class QueryFileReader {
 	private static final String QUERY_FILE_LOCATION = "inputs/queryFile.csv";
+	/*
 	private static final String DISCO_COMMAND = "java -jar ";
 	private static String DISCO_JAR;
 	private static String DISCO_REPO;
 	private static String DISCO_OPT;
+	*/
 	private static final String DELIMITER = ",";
 	private static long scores[][];
 	private static Vector<String> queries;
@@ -23,6 +25,7 @@ public class QueryFileReader {
 		queries = new Vector<String>();
 		queryTypes = new Vector<String>();
 		
+		/*
 		if (args.length != 3) {
 			System.out.println("Usage: java -jar Query.jar DISCO_JAR DISCO_REPO DISCO_OPT");
 			System.exit(1);
@@ -31,6 +34,7 @@ public class QueryFileReader {
 		DISCO_JAR = new String(args[0].toString());
 		DISCO_REPO = new String(args[1].toString());
 		DISCO_OPT = new String(args[2].toString());
+		*/
 		
 		/* Part 1: Read all input queries */
 		try {
@@ -101,7 +105,7 @@ public class QueryFileReader {
 		for (int i = 0; i < sWords.size(); i++) {
 			for (int j = 0; j < tWords.size(); j++) {
 				try {
-					child = Runtime.getRuntime().exec(DISCO_COMMAND + " " + DISCO_JAR + " " + DISCO_REPO + " " + DISCO_OPT + " " + sWords.get(i) + " " + tWords.get(j));
+					child = Runtime.getRuntime().exec("./ngd.py" + sWords.get(i) + " " + tWords.get(j) + "2>> err");
 					int result = child.waitFor();
 					
 					if (result == 0) {
@@ -144,3 +148,4 @@ public class QueryFileReader {
 		}
 	}
 }
+
