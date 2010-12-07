75af282f8340ff6ec40332d08c25b6024f29f4b3
aboSamoor <rmyeid@gmail.com>
2010-12-07 02:17:30 -0500

    formatting issues

--- downloader.rb
+++ downloader.rb
@@ -10,17 +10,15 @@ f = File.open('output', 'w')
 puts "Need to get #{to_get.length} queries ..!!"
 start_time = Time.now
 
-1000.times do
+50000.times do
   threads << Thread.new do
     while q_word = to_get.pop
       puts "remaining #{to_get.length} words" if to_get.length % 50 == 0
       if !cache[q_word]
-        res = false
-        while not res
-            if res = Net::HTTP.get('www.google.com', "/search?q=#{q_word}").match(/About ([\d+,*]+?) results/)
-                cache[q_word] = res.captures.first
-                f.puts "#{q_word},#{res.captures.first}"
-            end
+        if res = Net::HTTP.get('www.google.com', "/search?q=#{q_word}").match(/About ([\d+,*]+?) results/)
+            tmp = res.captures.first.gsub(",","")
+            cache[q_word] = tmp
+            f.puts "#{q_word.chop},#{tmp}"
         end
       end
     end 
