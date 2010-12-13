97dbd79d1acf9421e814d707cd5a1138a1b290d8
aboSamoor <rmyeid@gmail.com>
2010-12-13 11:58:10 -0500

    sinan modification on the ruby script

--- NGD/downloader.rb
+++ NGD/downloader.rb
@@ -6,33 +6,38 @@ require 'uri'
 cache = {}
 threads = []
 to_get = File.readlines(ARGV[0])
+dup = 0; dup_lock = Mutex.new
 
 f = File.open('output', 'w')
 
 puts "Need to get #{to_get.length} queries ..!!"
 start_time = Time.now
 
-500.times do
+1000.times do
   threads << Thread.new do
     while q_word = to_get.pop
       puts "remaining #{to_get.length} words" if to_get.length % 50 == 0
       if !cache[q_word]
         q_parsed = URI.parse(q_word)
-        if res = Net::HTTP.get(q_parsed.host, q_parsed.request_uri).match(/About ([\d+,*]+?) results/)
-            tmp = res.captures.first.gsub(",","")
-            cache[q_word] = tmp
-            f.flock(File::LOCK_EX)
-            f.puts "#{q_word.chop},#{tmp}\n"
-            f.flock(File::LOCK_UN)
-        end
-        end
+        res_http = Net::HTTP.get(q_parsed.host, q_parsed.request_uri)
+        result_stats = res_http.match(/<div[^>]*resultStats[^>]*>(.*?)<\/div>/)
+        tmp = result_stats ? result_stats[1].match(/([\d+,]+)/)[1].gsub(",","") : 0
+        cache[q_word] = tmp
+        f.flock(File::LOCK_EX)
+        f.puts "#{q_word.chop},#{tmp}\n"
+        f.flock(File::LOCK_UN)
+      else
+        dup_lock.synchronize { dup += 1 }
       end
-    end 
+    end
+  end 
 end
 
 
 threads.each { |x| x.join }
 puts "finished in #{Time.now - start_time}"
+puts "#{dup} duplicates found"
 puts "writing output ..."
 f.close()
 puts "Done."
+
